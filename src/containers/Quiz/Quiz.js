1ccac35a959d4006dbfe3e5eab02bb326577448b
tasabina <tasabina@yandex.ru>
2021-01-19 20:21:52 +1300

    Added menu. Finished mine functionality

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -5,6 +5,7 @@ import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz'
 
 class Quiz extends Component {
     state = {
+        results:{},
         isFinished: false,
         activeQuestion: 0,
         answerState: null,
@@ -42,18 +43,26 @@ class Quiz extends Component {
             }
         }
         const question = this.state.quiz[this.state.activeQuestion]
+        const results = this.state.results
 
         if(question.rightAnswerId === answerId) {
+
+            if(!results[question.id]) {
+                results[question.id] = 'success'
+            }
+
             this.setState({
                 answerState: {
                     [answerId] : 'success'
-                }
+                },
+                results
             })
             const timeout = window.setTimeout(()=>{
                 if (this.isQuizFinished()) {
                     this.setState({
                         isFinished: true
                     })
+                    console.log(this.state)
                 } else {
                     this.setState({
                         activeQuestion: this.state.activeQuestion + 1,
@@ -63,16 +72,27 @@ class Quiz extends Component {
                 window.clearTimeout(timeout)
             }, 1000)
         } else {
+            results[question.id] = 'error'
             this.setState({
                 answerState: {
                     [answerId] : 'error'
-                }
+                },
+                results
             })
         }
     }
 
     isQuizFinished() {
         return this.state.activeQuestion + 1 === this.state.quiz.length ? true : false
+    }
+
+    retryHandler = () => {
+        this.setState({
+            activeQuestion:0,
+            answerState:null,
+            isFinished: false,
+            results: {}
+        })
     }
 
     render() {
@@ -83,7 +103,11 @@ class Quiz extends Component {
 
                     {
                         this.state.isFinished
-                        ? <FinishedQuiz/>
+                        ? <FinishedQuiz
+                            results={this.state.results}
+                            quiz={this.state.quiz}
+                            onRetry={this.retryHandler}
+                        />
                         : <ActiveQuiz
                             answers={ this.state.quiz[this.state.activeQuestion].answers }
                             question={ this.state.quiz[this.state.activeQuestion].question }
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
 import classes from './Layout.module.css'
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle'
+import Drawer from '../../components/Navigation/Drawer/Drawer'
 
 class Layout extends Component {
+    state = {
+        menu: false
+    }
+    toogleMenuHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+    menuCloseHandler = () => {
+        this.setState({
+            menu: false
+        })
+    }
     render() {
         return (
         <div className={classes.Layout}>
+            <Drawer
+               isOpen={this.state.menu}
+               onClose={this.menuCloseHandler} 
+            />
+            <MenuToggle
+                onToggle={this.toogleMenuHandler}
+                isOpen={this.state.menu}
+            />
             <main>
                 {this.props.children}
             </main>
